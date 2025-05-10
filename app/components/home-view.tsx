7084121e80ae8d0b3bee2dbd80ec1b464c3a546d
Kyle <sangminem@gmail.com>
2025-05-10 21:54:03 +0900

    배경 스타일 변경

--- app/components/home-view.tsx
+++ app/components/home-view.tsx
@@ -4,21 +4,37 @@ import { Button, Stack, Typography } from "@mui/material";
 
 export const HomeView = () => {
   return (
-    <Stack sx={{ backgroundColor: "common.black", height: "100vh", width: "100vw", overflow: "hidden" }}>
-      <Typography variant="h1" color="success.light" sx={{ fontSize: "40px", fontWeight: 600, mx: "auto", p: 4 }}>
+    <Stack>
+      <Typography variant="h1" color="common.white" sx={{ fontSize: "40px", fontWeight: 600, mx: "auto", p: 4 }}>
         AI 생성 컴포넌트 샘플
       </Typography>
       <Stack gap={2} width={0.5} mx="auto">
-        <Button size="large" variant="outlined" color="success" sx={{ fontSize: "30px" }} onClick={() => {
+        <Button size="large" variant="outlined" color="inherit" sx={{
+          fontSize: "30px", color: "common.white", ":hover": {
+            opacity: 0.7
+          }
+        }} onClick={() => {
           location.href = "/sample1";
         }}>Sample1</Button>
-        <Button size="large" variant="outlined" color="success" sx={{ fontSize: "30px" }} onClick={() => {
+        <Button size="large" variant="outlined" color="inherit" sx={{
+          fontSize: "30px", color: "common.white", ":hover": {
+            opacity: 0.7
+          }
+        }} onClick={() => {
           location.href = "/sample2";
         }}>Sample2</Button>
-        <Button size="large" variant="outlined" color="success" sx={{ fontSize: "30px" }} onClick={() => {
+        <Button size="large" variant="outlined" color="inherit" sx={{
+          fontSize: "30px", color: "common.white", ":hover": {
+            opacity: 0.7
+          }
+        }} onClick={() => {
           location.href = "/sample3";
         }}>Sample3</Button>
-        <Button size="large" variant="outlined" color="success" sx={{ fontSize: "30px" }} onClick={() => {
+        <Button size="large" variant="outlined" color="inherit" sx={{
+          fontSize: "30px", color: "common.white", ":hover": {
+            opacity: 0.7
+          }
+        }} onClick={() => {
           location.href = "/sample4";
         }}>Sample4</Button>
       </Stack>
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { BackgroundGradientAnimation } from "@/app/components/background-gradient-animation";
 import { HomeView } from "@/app/components/home-view";
 import { SplashCursor } from "@/app/components/splash-cursor";
 
@@ -5,6 +6,7 @@ export default function Home() {
   return (
     <>
       <HomeView />
+      <BackgroundGradientAnimation />
       <SplashCursor />
     </>
   );
