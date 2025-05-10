eedee507b9812743b5da7867e61ceb2e4df74969
Kyle <sangminem@gmail.com>
2025-05-10 20:56:05 +0900

    splash cursor 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import { HomeView } from "@/app/components/home-view";
+import { SplashCursor } from "@/app/components/splash-cursor";
+import { Button, Stack } from "@mui/material";
+
 export default function Home() {
   return (
-    <main>
-      <div>Hello world!</div>
-    </main>
+    <>
+      <HomeView />
+      <SplashCursor />
+    </>
   );
 }
